5935bf784c548e22ef223de17e3397efcab59477
Hugo costa <hhugo.hcac@gmail.com>
2021-06-08 22:01:21 -0400

    first commit

---
+++ src/produto.models.ts
@@ -0,0 +1,14 @@
+export class Produto {
+
+id: number;
+codigo: string;
+nome: string;
+preco: number;
+
+constructor(codigo, nome, preco) {
+    this.codigo = codigo;
+    this.nome = nome;
+    this.preco = preco;
+  }
+
+}
